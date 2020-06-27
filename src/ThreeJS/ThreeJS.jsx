a37d41187437d353ccecdbe76be2499e26bc5c31
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-27 17:49:25 -0400

    l

--- src/ThreeJS/ThreeJS.jsx
+++ src/ThreeJS/ThreeJS.jsx
@@ -15,11 +15,11 @@ class ThreeJS extends Component {
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
     this.mount.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var geometry = new THREE.BoxGeometry(1, 1, 1); // changes the height, width, and length of the 3D object
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // changes the color the 0x means #
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 5;
+    camera.position.z = 5; // changes the position of the camera higher the number the farther away the object is
     var animate = function () {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01; // changes rotation left to right 0.00 only spins like a top
