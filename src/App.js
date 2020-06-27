21c8b54a1989b1093e203dbdcea2bdeb504daaec
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-27 16:39:53 -0400

    l

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ThreeJS from "./ThreeJS/ThreeJS";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <ThreeJS />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
